f21b248352b47ae50c89964b98427117dff25b21
DSJaswanth <88287856+DSJaswanth@users.noreply.github.com>
2021-08-03 00:17:35 +0530

    Add files via upload

---
+++ SCRIPT_Jaswanth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+##Write a script that prints your name, your email, your slack username (with @) and your biostack in that order.##
+print("Name:Jaswanth D S")
+print("Email:Jasdevshankar@gmail.com")
+print("stack username:@jaswanth")
+print("Biostack:GenomicS")
+
+
+# In[ ]:
+
+
+
+
