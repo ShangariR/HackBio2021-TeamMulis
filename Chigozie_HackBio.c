4ee0965bfc88c2fc1c90bab6a17d537ab81b3b51
Chigozie Nkwocha <bueze25@gmail.com>
2021-08-02 14:18:21 +0100

    Create Chigozie_HackBio.c
    This contains C-code for 2021 HackBio Internship(Week 1 Task)

---
+++ Chigozie_HackBio.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char name[30] = "Chigozie Nkwocha";
+    char email[80] = "bueze25@gmail.com";
+    char slack_username[80] = "GozieNkwocha";
+    char biostack[100] = "Transcriptomics";
+
+    printf("Name: %s\n", name);
+    printf("Email: %s\n", email);
+    printf("Slack Username: %s\n", slack_username);
+    printf("Biostack: %s\n", biostack);
+    return 0;
+}
+
