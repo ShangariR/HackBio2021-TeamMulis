a29decc2a54c6c1df32c0374f88a293ccc16619b
ShangariR <shangariariel5@gmail.com>
2021-08-02 18:25:18 +0530

    Create HackBio C++.cpp

---
+++ HackBio C++.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "My Name : Shangari. R\n";
+  cout << "My email id : shangariariel5@gmail.com\n";
+  cout << "My Slack username : @Shangari\n";
+  cout << "My Biostack : Genomics,Transcriptomics,Drug development\n";
+    return 0;
+}
