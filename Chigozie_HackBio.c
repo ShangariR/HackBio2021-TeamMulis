98d26d929aef2d565cfe1958e6787eed3b58c09d
Chigozie Nkwocha <bueze25@gmail.com>
2021-08-02 14:54:46 +0100

    Update Chigozie_HackBio.c

--- Chigozie_HackBio.c
+++ Chigozie_HackBio.c
@@ -4,7 +4,7 @@ int main(void)
 {
     char name[30] = "Chigozie Nkwocha";
     char email[80] = "bueze25@gmail.com";
-    char slack_username[80] = "GozieNkwocha";
+    char slack_username[80] = "@GozieNkwocha";
     char biostack[100] = "Transcriptomics";
 
     printf("Name: %s\n", name);
