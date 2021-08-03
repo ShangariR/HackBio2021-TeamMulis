4b12ad6475859a2d992c745357e746f42cd8e7c2
Chigozie Nkwocha <bueze25@gmail.com>
2021-08-03 14:37:40 +0100

    Raj_new
    Update from Raj.java to script_Rajarshi.java

--- script_Rajarshi.java
+++ script_Rajarshi.java
@@ -1,10 +1,6 @@
-
-public class Raj
-{
-   
-    public Raj()
-    {
-       System.out.println("My name is Rajarshi Mondal");
+public class script_Rajarshi {
+    public static void main(String args[]) {
+      System.out.println("My name is Rajarshi Mondal");
        System.out.println("Email ID: rajarshimondal92@gmail.com");
        System.out.println("Slack ID: @Rajarshi_Mondal");
        System.out.println("Biostack: Genomics and Transcriptomics");
