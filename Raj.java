5842d7ab6677ac5a368b3c82dc6bd9555913b5c2
Chigozie Nkwocha <bueze25@gmail.com>
2021-08-03 03:23:28 +0100

    new files

---
+++ Raj.java
@@ -0,0 +1,12 @@
+
+public class Raj
+{
+   
+    public Raj()
+    {
+       System.out.println("My name is Rajarshi Mondal");
+       System.out.println("Email ID: rajarshimondal92@gmail.com");
+       System.out.println("Slack ID: @Rajarshi_Mondal");
+       System.out.println("Biostack: Genomics and Transcriptomics");
+    }
+}
