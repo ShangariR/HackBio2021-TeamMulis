046378ee0299365af7d7c8360c908939218b671b
Chigozie Nkwocha <bueze25@gmail.com>
2021-08-04 20:16:50 +0100

    new_update
    Update to print as csv files

--- script_chigozie.c
+++ script_chigozie.c
@@ -5,9 +5,9 @@ int main(void)
 {
     char name[30] = "Chigozie Nkwocha";
     char email[80] = "bueze25@gmail.com";
-    char slack_username[80] = "GozieNkwocha";
+    char slack_username[80] = "@GozieNkwocha";
     char biostack[100] = "Transcriptomics";
-    char twitter[15] = "Gozi251";
+    char twitter[15] = "@Gozi251";
     int min_length;
     int hamming_distance = 0;
     int twitter_len = strlen(twitter);
@@ -28,12 +28,7 @@ int main(void)
             hamming_distance += 1;
         }
     }
-    printf("%s \n", name);
-    printf("%s \n", email);
-    printf("@%s \n", slack_username);
-    printf("%s \n", biostack);
-    printf("@%s\n", twitter);
-    printf("%i\n", hamming_distance);
+    printf("%s, %s, %s, %s, %s, %i", name, email, slack_username, biostack, twitter, hamming_distance);
     return 0;
 }
 
