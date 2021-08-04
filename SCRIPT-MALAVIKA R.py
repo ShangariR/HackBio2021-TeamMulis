d1b6712a69808f80dd12a3541542674714322662
Sadaf Raza <82495197+SadafRaza@users.noreply.github.com>
2021-08-04 18:26:28 +0530

    Update SCRIPT-MALAVIKA R.py

--- SCRIPT-MALAVIKA R.py
+++ SCRIPT-MALAVIKA R.py
@@ -1,4 +1,4 @@
-print("my name:R.Malavika")
-print("my email id:malavikapunitha0211@gmail.com")
-print("my slack username:@malavika")
-print("my biostack:genomics,data science,software development,drug development,vaccines development")
+print("Name: R.Malavika")
+print("Email: malavikapunitha0211@gmail.com")
+print("Slack username: @malavika")
+print("Biostack: genomics,data science,software development,drug development,vaccines development")
