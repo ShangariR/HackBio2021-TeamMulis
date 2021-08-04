29bf637b7f78f10fc38f384cab0a72b3f3d57ac1
Sadaf Raza <82495197+SadafRaza@users.noreply.github.com>
2021-08-04 19:00:11 +0530

    Update script_sadaf.R

--- script_sadaf.R
+++ script_sadaf.R
@@ -1,17 +1,17 @@
 ##Write a script that prints your name, your email, your slack username (with @) and your biostack in that order.##
 library(DescTools)
-slack_username <- "@sadaf"
-twitter_handle <- "@svdvf"
 name <- "Sadaf Raza"
 email <- "sadafraza48@gmail.com"
 biostack <- "Genomics"
+slack_username <- "@sadaf"
+twitter_handle <- "@svdvf"
 ## Hamming distance
 hamming_distance <- StrDist(slack_username, twitter_handle, method="hamming")
 
 
 data <- data.frame(Information=c(name, email, slack_username, biostack, twitter_handle, hamming_distance))
 
-rownames(data) <- c('NAME: ', 'E-MAIL: ', 'SLACK USERNAME: ', 'BIOSTACK: ', 'TWITTER HANDLE','HAMMING DISTANCE')
+rownames(data) <- c('NAME: ', 'E-MAIL: ', 'SLACK USERNAME: ', 'BIOSTACK: ', 'TWITTER HANDLE:','HAMMING DISTANCE:')
 
 colnames(data) <- NULL
 print(data)
