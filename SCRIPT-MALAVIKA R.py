88fc2d9c6631307e843b14be796267057b5e1e24
Malavika <88292204+MAlu02@users.noreply.github.com>
2021-08-05 12:08:42 +0530

    Add files via upload

--- SCRIPT-MALAVIKA R.py
+++ SCRIPT-MALAVIKA R.py
@@ -1,4 +1,16 @@
-print("Name: R.Malavika")
-print("Email: malavikapunitha0211@gmail.com")
-print("Slack username: @malavika")
-print("Biostack: genomics,data science,software development,drug development,vaccines development")
+print("my name:R.Malavika")
+print("my email id:malavikapunitha0211@gmail.com")
+print("my slack_sername:@malavika")
+print("my biostack:genomics,data science,software development,drug development,vaccines development")
+print("my twitter_Handle:@malavikq")
+
+#calculating the hamming distance between slack username and twitter username
+
+string1=slack_username("@malavika")
+string2=twitter_handle("@malavikq")
+hamming distance=0
+min length =min(len(string1),len(string2))
+for i in range(min length)
+if string1(i)!=string2(i)
+hamming distance =2
+print("hamming distance:2")
