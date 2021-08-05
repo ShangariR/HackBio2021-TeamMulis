90ae407f4494d2eb44fb4974a77760f0aa77a258
HVISHNUPRIYA <84326534+HVISHNUPRIYA@users.noreply.github.com>
2021-08-05 12:13:27 +0530

    Add files via upload

---
+++ script_Vishnupriya.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+use strict;
+
+print "My email id : vishna2000\@gmail.com\n";
+print "My slack_username : \@Vishnupriya\n";
+print "My Biostack : Genomics,Transcriptomics,Drug development\n";
+print "My twitter_handle : \@Vishnup7734\n";
+print "Hamming_distance : 4" ;
