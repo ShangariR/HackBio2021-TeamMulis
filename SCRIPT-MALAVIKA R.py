508af9ef43affd820bb17c17c5835db81173dd8b
Preeti <88307823+preetikprasad@users.noreply.github.com>
2021-08-06 19:08:54 +0530

    Update SCRIPT-MALAVIKA R.py

--- SCRIPT-MALAVIKA R.py
+++ SCRIPT-MALAVIKA R.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
-print("my name:R.Malavika")
-print("my email id:malavikapunitha0211@gmail.com")
-print("my slack_sername:@malavika")
-print("my biostack:genomics,data science,software development,drug development,vaccines development")
-print("my twitter_Handle:@malavikq")
 
-#calculating the hamming distance between slack username and twitter username
+Name = "R. Malavika"
+Email = "malavikapunitha0211@gmail.com"
+Slack_username = "@malavika"
+Biostack = "Genomics, data science, software development, drug development, vaccine development"
+Twitter_handle = "@malavikq"
 
-string1=slack_username("@malavika")
-string2=twitter_handle("@malavikq")
-@ m a l a v i k a ("string 1")
-@ m a l a v i k q ("string 2")
-hamming distance=0
-min length =min(len(string1),len(string2))
-# i denoted as position
-for i in range(min length)
-if string1(i)!=string2(i)
-hamming distance =1
-print("hamming distance:1")
+## calculating the hamming distance between stack_username and twitter handle##
+
+string1 = Slack_username
+string2 = Twitter_handle
+Hamming_distance = 0
+
+min_length = min(len(string1), len(string2))
+
+for i in range(min_length):
+    if string1[i] != string2[i]:
+        Hamming_distance += 1
+
+print("{}, {}, {}, {}, {}, {}".format(Name, Email, Slack_username, Biostack, Twitter_handle, Hamming_distance))
