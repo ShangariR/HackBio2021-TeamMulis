dbe3d3a31b649427afc96e3b2929130f42eb1db0
ShangariR <shangariariel5@gmail.com>
2021-08-06 21:24:22 +0530

    Clone repositoty in csv_generator

--- csv_generator.sh
+++ csv_generator.sh
@@ -2,8 +2,12 @@
 
 output="$1"
 
+git clone https://github.com/pragnapcu/HackBio2021-TeamMulis
+cd HackBio2021-TeamMulis
+
 (
 g++ script_Shangari.cpp -o script_Shangari && ./script_Shangari
 perl script_Vishnupriya.pl
 python3 script_Preeti.py
-) > "$output"
+) > ../"$output"
+
