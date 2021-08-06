198feb6f272705714470e448c1e43cbb649263ce
Preeti <88307823+preetikprasad@users.noreply.github.com>
2021-08-06 18:29:27 +0530

    Update script_Nasrin.py

--- script_Nasrin.py
+++ script_Nasrin.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
-name="Nasrin Parvin"
-email="p.nasrin520@gmail.com"
-slack_username="@Nasrin"
-biostack="Genomics"
-twitter_handle="@Nasrin123"
 
+Name = "Nasrin Parvin"
+Email = "p.nasrin520@gmail.com"
+Slack_username = "@Nasrin"
+Biostack = "Genomics"
+Twitter_handle = "@Nasrin123"
 
 ## calculating the hamming distance between stack_username and twitter handle##
 
-string1 = slack_username
-string2= twitter_handle
-hamming_distance = 0
+string1 = Slack_username
+string2 = Twitter_handle
+Hamming_distance = 0
 
 min_length = min(len(string1), len(string2))
 
 for i in range(min_length):
-     if string1[i] != string2[i]:
+    if string1[i] != string2[i]:
         hamming_distance += 1
-      
-    
-print("Name:"+name,'Email:'+email,'Slack username:'+slack_username,'Biostack:'+biostack,'Twitter username :'+twitter_handle,hamming_distance,sep= "\n")
+
+print("{}, {}, {}, {}, {}, {}".format(Name, Email, Slack_username, Biostack, Twitter_handle, Hamming_distance))
